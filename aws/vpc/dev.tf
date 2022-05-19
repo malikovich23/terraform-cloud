92ae63715316360c1c4a7473d563dc987e521520
malikovich23 <46453825+malikovich23@users.noreply.github.com>
2022-05-19 15:01:46 -0400

    Update dev.tf

--- aws/vpc/dev.tf
+++ aws/vpc/dev.tf
@@ -5,9 +5,6 @@ module "vpc" {
   public_cidr1  = "10.0.1.0/24"
   public_cidr2  = "10.0.2.0/24"
   public_cidr3  = "10.0.3.0/24"
-  private_cidr1 = "10.0.101.0/24"
-  private_cidr2 = "10.0.102.0/24"
-  private_cidr3 = "10.0.103.0/24"
   tags = {
     Name        = "VPC_Project"
     Environment = "Dev"
@@ -28,15 +25,7 @@ output "public_subnet2" {
 output "public_subnet3" {
   value = module.vpc.public_subnets[2]
 }
-output "private_subnet1" {
-  value = module.vpc.private_subnets[0]
-}
-output "private_subnet2" {
-  value = module.vpc.private_subnets[1]
-}
-output "private_subnet3" {
-  value = module.vpc.private_subnets[2]
-}
+
 output "vpc" {
   value = module.vpc.vpc
 }
